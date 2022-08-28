12a48fb15c3ff5118558c7638df7e708f8c4afe5
jackliu333 <jackliu333@gmail.com>
2022-08-29 06:01:05 +0800

    upsize fonts

--- Chapter_5/chapter_5.R
+++ Chapter_5/chapter_5.R
@@ -1,4 +1,5 @@
 library(readr)
+library(dplyr)
 
 df = read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/comic-characters/marvel-wikia-data.csv")
 df
@@ -18,7 +19,12 @@ table(df$ALIGN, df$SEX)
 
 library(ggplot2)
 ggplot(df, aes(x=SEX, fill=ALIGN)) +
-  geom_bar()
+  geom_bar() + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.position = c(0.2, 0.8),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=20)) 
 
 df %>% 
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
@@ -48,13 +54,21 @@ df %>%
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
   ggplot(aes(x=SEX, fill=ALIGN)) +
   geom_bar(position="fill") +
-  ylab("proportion")
+  ylab("proportion") + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=20)) 
 
 df %>% 
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
   ggplot(aes(x=ALIGN, fill=SEX)) +
   geom_bar(position="fill") +
-  ylab("proportion")
+  ylab("proportion") + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=20)) 
 
 
 # Marginal distribution and faceted bar chart
@@ -65,33 +79,50 @@ df %>%
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
   ggplot(aes(x=SEX)) +
   geom_bar() +
-  facet_wrap(~ALIGN)
+  facet_wrap(~ALIGN) + 
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=15,face="bold"),
+        strip.text.x = element_text(size = 30)) 
 
 df$ALIGN = factor(df$ALIGN, levels = c("Bad Characters", "Neutral Characters", "Good Characters"))
 df %>% 
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
   ggplot(aes(x=SEX)) +
   geom_bar() +
-  facet_wrap(~ALIGN)
+  facet_wrap(~ALIGN) + 
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=15,face="bold"),
+        strip.text.x = element_text(size = 30))
 
 ###### EXERCISE 5.3 ######
 summary(df$Year)
 
 ggplot(df, aes(x=Year)) +
-  geom_dotplot(dotsize=0.2)
+  geom_dotplot(dotsize=0.2) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold")) 
 
 ggplot(df, aes(x=Year)) +
-  geom_histogram()
+  geom_histogram() + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold")) 
 
 ggplot(df, aes(x=Year)) +
-  geom_density()
+  geom_density() + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold")) 
 
 ggplot(df, aes(x=Year)) +
-  geom_boxplot()
+  geom_boxplot() + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold")) 
 
 ggplot(df, aes(x=Year)) +
   geom_boxplot() +
-  facet_wrap(~SEX)
+  facet_wrap(~SEX) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        strip.text.x = element_text(size = 30)) 
 
 ggplot(df, aes(x=APPEARANCES)) +
   geom_boxplot()
@@ -102,13 +133,21 @@ df %>%
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
   ggplot(aes(x=Year)) +
   geom_density() +
-  facet_grid(ALIGN ~ SEX, labeller = label_both)
+  facet_grid(ALIGN ~ SEX, labeller = label_both) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        strip.text.x = element_text(size = 30),
+        strip.text.y = element_text(size = 12)) 
 
 df %>% 
   filter(!(SEX %in% c("Agender Characters", "Genderfluid Characters"))) %>% 
   ggplot(aes(x=Year)) +
   geom_histogram() +
-  facet_grid(ALIGN ~ SEX, labeller = label_both)
+  facet_grid(ALIGN ~ SEX, labeller = label_both) +
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        strip.text.x = element_text(size = 30),
+        strip.text.y = element_text(size = 12)) 
 
 ###### EXERCISE 5.5 ######
 mean(df$APPEARANCES)
@@ -158,16 +197,31 @@ tmp = df %>%
   filter(Year >= 2000)
 
 ggplot(tmp, aes(x=APPEARANCES, fill=ALIGN)) + 
-  geom_density(alpha=0.2)
+  geom_density(alpha=0.2) +
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.position = c(0.8, 0.8),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=20))
 
 tmp = tmp %>% 
   filter(APPEARANCES <= quantile(APPEARANCES, 0.9, na.rm=TRUE))
 
 ggplot(tmp, aes(x=APPEARANCES, fill=ALIGN)) + 
-  geom_density(alpha=0.2)
+  geom_density(alpha=0.2) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.position = c(0.8, 0.8),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=20))
 
 ggplot(tmp, aes(x=log(APPEARANCES), fill=ALIGN)) + 
-  geom_density(alpha=0.2)
+  geom_density(alpha=0.2) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.position = c(0.8, 0.8),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=20))
 
 
 ###### EXERCISE 5.8 ######
@@ -176,7 +230,7 @@ library(yfR)
 
 first_date = as.Date("2021-01-01")
 last_date = as.Date("2022-01-01")
-my_ticker <- c('FB', 'NFLX', 'GOOG', 'AMZN', 'MSFT')
+my_ticker <- c('META', 'NFLX', 'GOOG', 'AMZN', 'MSFT')
 
 df <- yf_get(tickers = my_ticker, 
                          first_date = first_date,
@@ -187,16 +241,28 @@ str(df)
 ggplot(df, 
        aes(x = ref_date, y = price_adjusted,
            color = ticker)) + 
-  geom_line()
+  geom_line() + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.text = element_text(size=20))
 
 ggplot(df, aes(x=price_adjusted, fill=ticker)) +
-  geom_histogram(bins=100) 
+  geom_histogram(bins=100) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.text = element_text(size=20))
 
 ggplot(df, aes(x=price_adjusted, fill=ticker)) +
-  geom_density(alpha=0.2) 
+  geom_density(alpha=0.2) + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.text = element_text(size=20))
 
 ggplot(df, aes(ticker, price_adjusted, fill=ticker)) +
-  geom_boxplot() 
+  geom_boxplot() + 
+  theme(axis.text=element_text(size=18),
+        axis.title=element_text(size=18,face="bold"),
+        legend.text = element_text(size=20))
 
 df %>% 
   group_by(ticker) %>% 
